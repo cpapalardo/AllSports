95773b3ac34e9c87b44053f5e9acd793e6567baa
Marco <marco.031@hotmail.com>
2016-06-03 20:38:54 -0300

    teste

--- AllSports/Utils/Sql.cs
+++ AllSports/Utils/Sql.cs
@@ -7,7 +7,10 @@ using System.Data;
 namespace AllSports.Utils {
 	public class Sql {
 		public static SqlConnection Open() {
-			SqlConnection conn = new SqlConnection("string de conexão");
+			SqlConnection conn = new SqlConnection("Server=tcp:allsports.database.windows.net,"
+			+ "1433;Database=allsports;User ID=allsports@allsports;"
+			+ "Password=Bandtec2016;Encrypt=True;TrustServerCertificate=False;"
+			+ "Connection Timeout=30;");
 			conn.Open();
 			return conn;
 		}
